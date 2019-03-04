88e063261cd187d3eda34c164c1cc4aca94d6801
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-03-04 17:26:11 +0100

    notary: do signature verification and hash the content before storing it to bcdb

--- packages/notary/actors/notary_actor.py
+++ packages/notary/actors/notary_actor.py
@@ -1,62 +1,72 @@
-
 import json
 from Jumpscale import j
+import nacl
 from pyblake2 import blake2b
+
 
 JSBASE = j.application.JSBaseClass
 
 
 class notary_actor(JSBASE):
     def __init__(self):
-
         self.bcdb = j.data.bcdb.bcdb_instances["notary_bcdb"]
+        self.tfchain = j.clients.tfchain.new('notary', network_type='TEST')
 
     def start_page(self):
         return "welcome to https://github.com/threefoldtech/home/issues/135"
 
-    def register(self, threebot_id, key, content, content_signature, schema_out):
+    def _bot_verify_key(self, bot_id):
+        record = self.tfchain.threebot.record_get(bot_id)
+        encoded_key = record.public_key.hash
+        return nacl.signature.VerifyKey(
+            str(encoded_key),
+            encoder=nacl.encoding.HexEncoder)
+
+    def register(self, threebot_id, content, content_signature, schema_out):
         """
         ```in
         threebot_id = "" (S)
-        key = "" (S)
-        content = "" (S)
-        content_signature = (S)
+        content = "" (bytes)
+        content_signature = (bytes)
         ```
         ```out
-        message = "" (S)
+        hash = "" (S)
         ```
         """
-        out = schema_out.new()
-        bcdb = self.bcdb
-        model = bcdb.models.get("threefold.grid.notary")
-        # TODO: check that signature is inline with 3Bot ID provided, meaning the Notary verifies that the 3bot with the ID sent has indeed created the info
+        verify_key = self._bot_verify_key(threebot_id)
+        _verify_signature(content_signature, verify_key)
+
+        model = self.bcdb.models.get("threefold.grid.notary.reservation")
+
+        content_hash = _hash_content(content)
+
         model_obj = model.new()
+        model_obj.hash = content_hash
         model_obj.threebot_id = threebot_id
-        model_obj.key = key
         model_obj.content = content
         model_obj.content_signature = content_signature
         model_obj.save()
 
-        content_hash = blake2b(str(model_obj).encode(), digest_size=10).hexdigest()
-        out.message = "Hash of the content is {}".format(content_hash)
+        out = schema_out.new()
+        out.hash = content_hash
         return out
 
-    def get(self, key, schema_out):
+    def get(self, hash, schema_out):
         """
         ```in
-        key = "" (S)
+        hash = "" (S)
         ```
         ```out
-        message = "" (S)
+        !threefold.grid.notary.reservation
         ```
         """
-        out = schema_out.new()
-        bcdb = self.bcdb
-        for model in bcdb.get_all():
-            if model.key == key:
-                out.message = "key:{} , content:{} , threebot_id:{} , content_signature:{}".format(
-                    model.key, model.content, model.threebot_id, model.content_signature)
-                return out
+        model = self.bcdb.models.get("threefold.grid.notary.reservation")
+        return model.get(hash=hash)
 
-        out.message = "this key doesn't exist"
-        raise Exception(out.message)
+
+def _hash_content(content):
+    return j.data.blake2_string(content, size=32)
+
+
+def _verify_signature(signature, verify_key):
+    verify_key.verify(signature)
