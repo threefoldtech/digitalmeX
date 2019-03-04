0dbdaf499e74ca3d9d9f5f8def22929a228d3941
andrewayoub <andrew.a.mikhaeel@gmail.com>
2019-03-04 17:18:40 +0200

    add name param to webhook actor

--- packages/system/base/actors/webhook.py
+++ packages/system/base/actors/webhook.py
@@ -2,13 +2,16 @@ from Jumpscale import j
 
 
 class webhook(j.application.JSBaseClass):
-    def pull_repo(self, url):
+    def pull_repo(self, url, name):
         """
         ```in
         url = ""
+        name = ""
         ```
         :param url: git url of the repository you need to pull
         :return:
         """
         j.clients.git.pullGitRepo(url)
+        threefold_docsite = j.tools.markdowndocs.load(url, name=name)
+        threefold_docsite.write()
         return True
