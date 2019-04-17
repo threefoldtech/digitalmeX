bc1586f55da9c5316def5aeb629345bf817de1ed
Ahmed T. Youssef <xmonader@gmail.com>
2019-04-17 12:22:44 +0200

    pass static dir to gdrive actor

--- packages/gdrive/actors/gdrive.py
+++ packages/gdrive/actors/gdrive.py
@@ -23,7 +23,7 @@ class gdrive(j.application.JSBaseClass):
         doctypes_map = {
             'doc': 'drive',
             'slide': 'slides',
-         }
+        }
         #
         # http: // $IPADDR / gdrive / slide /$gslide_slide_guid.png
         # http: // $IPADDR / gdrive / slide /$gpresentation_guid /$slidename.png (LATER)
@@ -41,11 +41,10 @@ class gdrive(j.application.JSBaseClass):
         assert doctype in doctypes_map
         service_name = doctypes_map[doctype]
         if doctype == "doc":
-            gfile = cl.getFile(guid, service_name=service_name, service_version="v3")
-            gfile.exportPDF(self, path=j.sal.fs.joinPaths(STATIC_DIR, "gdrive", doctype, "{guid}.pdf".format(guid)))
+            gfile = cl.getFile(
+                guid, service_name=service_name, service_version="v3")
+            gfile.exportPDF(self, path=j.sal.fs.joinPaths(
+                STATIC_DIR, "gdrive", doctype, "{guid}.pdf".format(guid)))
         elif doctype == "slide":
-            cl.exportSlides(guid, "/tmp/exportedslides/")
+            cl.exportSlides(guid, "/tmp/exportedslides/", staticdir=STATIC_DIR)
             # rename all the slides in exportedslides/$prsentation_name/
-
-
-
