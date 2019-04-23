1df8817c34260f3d89183a8c938cfbfe181113b4
ahmedhanafy725 <ahmed.hanafy725@gmail.com>
2019-04-23 12:05:08 +0000

    some edit in gdrive actor test

--- packages/gdrive/tests/README.md
+++ packages/gdrive/tests/README.md
@@ -1,10 +1,10 @@
-#### Before run this test:
+#### Before running this test:
 
-1- install requirement `pip3 install -r requirements.txt`.
+1- Install requirement `pip3 install -r requirements.txt`.
 
-2- Google cerdential file should add to current directory under name `cred.json`.
+2- Google credential file should be added to current directory under name `cred.json`.
 
-3- Add endpoints need to be tested `pdf_links.txt`, `png_links.txt`.
+3- Add endpoints need to be tested in `pdf_links.txt`, `png_links.txt`.
 
 #### To run test:
 ```bash
--- packages/gdrive/tests/testcases.py
+++ packages/gdrive/tests/testcases.py
@@ -10,8 +10,8 @@ with open('pdf_links.txt', 'r') as f:
 
 png_urls = list()
 pdf_urls = list()
-[png_urls.append(i) for i in png_links if i not in ['\n', '']]
-[pdf_urls.append(i) for i in pdf_links if i not in ['\n', '']]
+[png_urls.append(i.strip('\n')) for i in png_links if i not in ['\n', '']]
+[pdf_urls.append(i.strip('\n')) for i in pdf_links if i not in ['\n', '']]
 
 
 class Testcases(Basetest):
@@ -23,16 +23,16 @@ class Testcases(Basetest):
         **Test Scenario:**
 
         #. Get request to a url.
-        #. Check that url is exists.
-        #. Check the content type is png.
+        #. Check that url exists.
+        #. Check that the content type is png.
         """
-        self.log('Get request to a url {}'.format(url))
-        req = requests.get(url.strip('\n'))
+        self.log('Get request to url {}'.format(url))
+        req = requests.get(url)
         
-        self.log('Check that url is exists')
+        self.log('Check that url exists')
         self.assertEquals(req.status_code, requests.codes.ok, "Couldn't find this url")
 
-        self.log('Check the content type is png')
+        self.log('Check that the content type is png')
         content_type = req.headers['content-Type']
         self.assertEquals(content_type, 'image/png', "This url is not contains a png")
 
@@ -43,15 +43,15 @@ class Testcases(Basetest):
         **Test Scenario:**
 
         #. Get request to a url.
-        #. Check that url is exists.
-        #. Check the content type is pdf.
+        #. Check that url exists.
+        #. Check that the content type is pdf.
         """
         self.log('Get request to a url {}'.format(url))
-        req = requests.get(url.strip('\n'))
+        req = requests.get(url)
 
-        self.log('Check that url is exists')
+        self.log('Check that url exists')
         self.assertEquals(req.status_code, requests.codes.ok, "Couldn't find this url")
 
-        self.log('Check the content type is pdf')
+        self.log('Check that the content type is pdf')
         content_type = req.headers['content-Type']
         self.assertEquals(content_type, 'application/pdf', "This url is not contains a pdf")
