50cbefe6019d1355ee5558286031243ddcb68081
Reem Khamis <khamisr@codescalers.com>
2019-07-30 17:47:59 +0200

    Make sure lualibs are installed in openresty

--- DigitalMe/servers/openresty/OpenRestyServer.py
+++ DigitalMe/servers/openresty/OpenRestyServer.py
@@ -55,6 +55,9 @@ class OpenRestyServer(j.application.JSBaseConfigsConfigFactoryClass):
         # copy the templates to the right location
         j.sal.fs.copyDirTree("%s/web_resources/" % self._dirpath, self._web_path)
         # link individual files & create a directory TODO:*1
+
+        j.sal.fs.copy("%s/web_resources/lualib/redis.lua" % self._dirpath, "/sandbox/openresty/liblua/")
+        j.sal.fs.copy("%s/web_resources/lualib/websocket.lua" % self._dirpath, "/sandbox/openresty/liblua/")
         self.status = "installed"
 
         self.save()
