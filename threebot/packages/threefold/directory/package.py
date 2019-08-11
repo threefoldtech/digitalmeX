a6a9cad08f68fb5c4bc8443aa36c3fc1b1dd585f
kristof de spiegeleer <>
2019-08-11 08:54:14 +0200

    package

--- threebot/packages/threefold/directory/package.py
+++ threebot/packages/threefold/directory/package.py
@@ -1,32 +1,32 @@
+from Jumpscale import j
 
- from Jumpscale import j
 
- class Package(j.application.ThreeBotPackageBase):
+class Package(j.application.ThreeBotPackageBase):
+    def prepare(self):
+        """
+        is called at install time
+        :return:
+        """
+        j.shell()
+        pass
 
-     def prepare(self):
-         """
-         is called at install time
-         :return:
-         """
-         pass
+    def start(self):
+        """
+        called when the 3bot starts
+        :return:
+        """
+        pass
 
-     def start(self):
-         """
-         called when the 3bot starts
-         :return:
-         """
-         pass
+    def stop(self):
+        """
+        called when the 3bot stops
+        :return:
+        """
+        pass
 
-     def stop(self):
-         """
-         called when the 3bot stops
-         :return:
-         """
-         pass
-
-     def delete(self):
-         """
-         called when the package is no longer needed and will be removed from the threebot
-         :return:
-         """
-         pass
+    def delete(self):
+        """
+        called when the package is no longer needed and will be removed from the threebot
+        :return:
+        """
+        pass
