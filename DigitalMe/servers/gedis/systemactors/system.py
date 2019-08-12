d232e3c24a5cbe4c293e19fafa16179e23a62ed6
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-08-12 08:19:06 +0200

    gedis server: send all know schema to client (temporary hack)

--- DigitalMe/servers/gedis/systemactors/system.py
+++ DigitalMe/servers/gedis/systemactors/system.py
@@ -19,24 +19,30 @@ class system(JSBASE):
         return all core schemas as understood by the server, is as text, can be processed by j.data.schema
         """
         out = ""
-        urls = [
-            "jumpscale.bcdb.user.2",
-            "jumpscale.bcdb.circle.2",
-            "jumpscale.bcdb.acl.2",
-            "jumpscale.bcdb.acl.circle.2",
-            "jumpscale.bcdb.acl.user.2",
-            "jumpscale.bcdb.meta.2",
-            "jumpscale.bcdb.meta.schema.2",
-            "jumpscale.bcdb.namespace.2",
-            "jumpscale.gedis.server",
-            "jumpscale.gedis.api",
-            "jumpscale.gedis.cmd",
-            "jumpscale.gedis.schema",
-        ]
-        for url in urls:
-            md5 = j.data.schema.url_to_md5[url][-1]
-            s = j.data.schema.md5_to_schema[md5]
-            out += "%s\n\n" % s.text
+        # urls = [
+        #     "jumpscale.bcdb.user.2",
+        #     "jumpscale.bcdb.circle.2",
+        #     "jumpscale.bcdb.acl.2",
+        #     "jumpscale.bcdb.acl.circle.2",
+        #     "jumpscale.bcdb.acl.user.2",
+        #     "jumpscale.bcdb.meta.2",
+        #     "jumpscale.bcdb.meta.schema.2",
+        #     "jumpscale.bcdb.namespace.2",
+        #     "jumpscale.gedis.server",
+        #     "jumpscale.gedis.api",
+        #     "jumpscale.gedis.cmd",
+        #     "jumpscale.gedis.schema",
+        # ]
+
+        # FIXME: this now sends all the of the know schema to the client
+        # we should find a way to only send the schema that the gedis server actually uses
+        # in order to minimize the amount of data we have to send to the client
+        # at each connections
+
+        for md5s in j.data.schema.url_to_md5.values():
+            for md5 in md5s:
+                s = j.data.schema.md5_to_schema[md5]
+                out += "%s\n\n" % s.text
         return out
 
     def actors_add_path(self, namespace, path):
