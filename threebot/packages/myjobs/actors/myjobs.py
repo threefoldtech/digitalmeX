e0fb6b2b1a371ad28ae2489bfc84b468e8a14d40
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-08-22 13:29:49 +0200

    return demo data until myjobs server is done

--- threebot/packages/myjobs/actors/myjobs.py
+++ threebot/packages/myjobs/actors/myjobs.py
@@ -1,37 +1,186 @@
 from Jumpscale import j
 
 
+WORKERS = """
+{
+"workers": [
+{
+"timeout": "3,600",
+"time_start": "2019/08/21 15:57:51",
+"last_update": "2019/08/21 16:10:03",
+"current_job": "-",
+"error": "",
+"state": "NEW",
+"pid": "0",
+"halt": false,
+"id": 1
+},
+{
+"timeout": "3,600",
+"time_start": "2019/08/21 15:57:54",
+"last_update": "2019/08/21 15:57:54",
+"current_job": "-",
+"error": "",
+"state": "new",
+"pid": "0",
+"halt": false,
+"id": 2
+},
+{
+"timeout": "3,600",
+"time_start": "2019/08/21 15:57:56",
+"last_update": "2019/08/21 16:03:42",
+"current_job": "-",
+"error": "",
+"state": "NEW",
+"pid": "0",
+"halt": false,
+"id": 3
+}
+]
+}
+"""
+
+
+JOBS = """
+{
+"jobs": [
+{
+"category": "",
+"time_start": "2019/08/21 16:01:41",
+"time_stop": "2019/08/21 16:01:41",
+"state": "OK",
+"timeout": "0",
+"action_id": "4",
+"args": "[\n1,\n2\n]",
+"kwargs": "{}",
+"result": "3",
+"error": "{}",
+"return_queues": [],
+"id": 5
+},
+{
+"category": "",
+"time_start": "2019/08/21 16:03:42",
+"time_stop": "2019/08/21 16:05:22",
+"state": "OK",
+"timeout": "0",
+"action_id": "6",
+"args": "[\n1,\n2\n]",
+"kwargs": "{}",
+"result": "3",
+"error": "{}",
+"return_queues": [],
+"id": 7
+},
+{
+"category": "",
+"time_start": "2019/08/21 16:10:03",
+"time_stop": "2019/08/21 16:11:43",
+"state": "OK",
+"timeout": "0",
+"action_id": "6",
+"args": "[\n1,\n2\n]",
+"kwargs": "{}",
+"result": "3",
+"error": "{}",
+"return_queues": [],
+"id": 8
+}
+]
+}
+print(getWorkers())
+undefined
+VM5682:12 {
+"workers": [
+{
+"timeout": "3,600",
+"time_start": "2019/08/21 15:57:51",
+"last_update": "2019/08/21 16:10:03",
+"current_job": "-",
+"error": "",
+"state": "NEW",
+"pid": "0",
+"halt": false,
+"id": 1
+},
+{
+"timeout": "3,600",
+"time_start": "2019/08/21 15:57:54",
+"last_update": "2019/08/21 15:57:54",
+"current_job": "-",
+"error": "",
+"state": "new",
+"pid": "0",
+"halt": false,
+"id": 2
+},
+{
+"timeout": "3,600",
+"time_start": "2019/08/21 15:57:56",
+"last_update": "2019/08/21 16:03:42",
+"current_job": "-",
+"error": "",
+"state": "NEW",
+"pid": "0",
+"halt": false,
+"id": 3
+}
+]
+}
+"""
+
+
 class myjobs(j.application.ThreeBotActorBase):
     def _init(self, **kwargs):
         self.worker_model = j.servers.myjobs.model_worker
         self.job_model = j.servers.myjobs.model_job
+        self.action_model = j.servers.myjobs.model_action
 
-    def list_workers(self, schema_out):
-        """
-        ```in
-        ```
+    # def list_workers(self, schema_out):
+    #     """
+    #     ```in
+    #     ```
 
-        ```out
-        workers = (LO) !jumpscale.myjobs.worker
-        ```
-        """
-        output = schema_out.new()
-        for worker in self.worker_model.iterate():
-            output.workers.append(worker)
+    #     ```out
+    #     workers = (LO) !jumpscale.myjobs.worker
+    #     ```
+    #     """
+    #     out = schema_out.new()
+    #     out.workers = list(self.worker_model.iterate())
+    #     return out
 
-        return output
+    # def list_jobs(self, schema_out):
+    #     """
+    #     ```in
+    #     ```
 
-    def list_jobs(self, schema_out):
-        """
-        ```in
-        ```
+    #     ```out
+    #     jobs = (LO) !jumpscale.myjobs.job
+    #     ```
+    #     """
+    #     out = schema_out.new()
+    #     out.jobs = list(self.job_model.iterate())
+    #     return out
 
-        ```out
-        jobs = (LO) !jumpscale.myjobs.job
-        ```
-        """
-        output = schema_out.new()
-        for job in self.job_model.iterate():
-            output.jobs.append(job)
+    # def list_actions(self, schema_out):
+    #     """
+    #     ```in
+    #     ```
 
-        return output
+    #     ```out
+    #     actions = (LO) !jumpscale.myjobs.action
+    #     ```
+    #     """
+    #     out = schema_out.new()
+    #     out.actions = list(self.action_model.iterate())
+    #     return out
+
+    def list_workers(self):
+        return WORKERS
+
+    def list_action(self):
+        return []
+
+    def list_jobs(self):
+        return JOBS
