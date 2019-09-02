5376bc49eab2f6092880310c119a410338c91664
= <hamdy.a.farag@gmail.com>
2019-09-02 06:54:31 +0200

    webdav - bcdb completed

--- DigitalMe/servers/webdav/WebdavServer.py
+++ DigitalMe/servers/webdav/WebdavServer.py
@@ -1,7 +1,15 @@
 from Jumpscale import j
 
-from .app import App
+from wsgidav.wsgidav_app import WsgiDAVApp
+from gevent.pywsgi import WSGIServer
+from wsgidav.debug_filter import WsgiDavDebugFilter
+from wsgidav.dir_browser import WsgiDavDirBrowser
+from wsgidav.error_printer import ErrorPrinter
+from wsgidav.http_authenticator import HTTPAuthenticator
+from wsgidav.request_resolver import RequestResolver
 
+from .provider import BCDBFSProvider
+# from Jumpscale.data.bcdb.connectors.webdav.BCDBFSProvider import BCDBFSProvider
 JSConfigClient = j.application.JSBaseConfigClass
 
 
@@ -12,12 +20,55 @@ class WebdavServer(JSConfigClient):
        port = 6661 (I)
        """
 
+    _app = None
+
+    def get_app(self, name="webdav", path="/", port=6661, user_mapping={}, debug=False):
+        if not self._app:
+
+            # def addUser(realmName, user, password, description, roles=[]):
+            #     realmName = "/" + realmName.strip(r"\/")
+            #     userDict = user_mapping.setdefault(realmName, {}).setdefault(user, {})
+            #     userDict["password"] = password
+            #     userDict["description"] = description
+            #     userDict["roles"] = roles
+            # if user_mapping == {}:
+            #     addUser("", "root", "root", "")
+
+            if not j.data.bcdb.exists('fs'):
+                j.data.bcdb.new('fs')
+
+            config = {
+                "host": "0.0.0.0",
+                "port": port,
+                "provider_mapping": {"/": BCDBFSProvider(path)},
+                "verbose": 5,
+                "middleware_stack": [
+                    WsgiDavDebugFilter,
+                    ErrorPrinter,
+                    HTTPAuthenticator,
+                    WsgiDavDirBrowser,  # configured under dir_browser option (see below)
+                    RequestResolver,  # this must be the last middleware item
+                ],
+                "error_printer": {"catch_all": True},  # False,
+                "enable_loggers": ['wsgidav'],
+                "simple_dc": {"user_mapping": user_mapping or {"*": True}},
+            }
+
+            if debug:
+                config['middleware_stack'].pop(0)
+                config['middleware_stack'].pop(0)
+
+            self._app = WsgiDAVApp(config)
+            self._app.debug = True
+
+        return self._app
+
     def start(self, background=False):
         self.install()
         if not background:
             rack = j.servers.rack.get()
-            rack.webdav_server_add(name=self.name, path="/", port=self.port, webdavprovider=None)
-            rack.bottle_server_add(name="fileman", port=self.port, app=app)
+            server = WSGIServer(("0.0.0.0", self.port), application=self.get_app())
+            rack.add(name=self.name, server=server)
             rack.start()
 
     def install(self):
--- DigitalMe/servers/webdav/provider.py
+++ DigitalMe/servers/webdav/provider.py
@@ -20,10 +20,9 @@ from wsgidav.dav_error import DAVError, HTTP_FORBIDDEN
 from wsgidav.dav_provider import DAVCollection, DAVNonCollection, DAVProvider
 
 import os
-import shutil
-import stat
 import sys
 
+from Jumpscale import j
 
 __docformat__ = "reStructuredText"
 
@@ -32,41 +31,67 @@ _logger = util.get_module_logger(__name__)
 BUFFER_SIZE = 8192
 
 
+class IOInterface(object):
+    def __init__(self, db, filename):
+        self.db = db
+        self.filename = filename
+
+    def tell(self):
+        raise NotImplementedError
+
+    def read(self, size=-1):
+        raise NotImplementedError
+
+    def seek(self, offset, whence=0):
+        raise NotImplementedError
+
+    @property
+    def name(self):
+        raise NotImplementedError
+
+    def write(self, data):
+        self.db.file_write(self.filename, data)
+
+    def truncate(self, size=None):
+        raise NotImplementedError
+
+    def flush(self):
+        raise NotImplementedError
+
+    def fileno(self):
+        raise NotImplementedError
+
 # ========================================================================
 # FileResource
 # ========================================================================
+
+
 class FileResource(DAVNonCollection):
-    """Represents a single existing DAV resource instance.
-
-    See also _DAVResource, DAVNonCollection, and FilesystemProvider.
-    """
-
     def __init__(self, path, environ, file_path):
         super(FileResource, self).__init__(path, environ)
         self._file_path = file_path
-        self.file_stat = os.stat(self._file_path)
-        # Setting the name from the file path should fix the case on Windows
         self.name = os.path.basename(self._file_path)
         self.name = compat.to_native(self.name)
+        self.db = j.sal.bcdbfs
+        self.obj = self.db._file_model.get_by_name(name=self._file_path)[0]
 
-    # Getter methods for standard live properties
     def get_content_length(self):
-        return self.file_stat[stat.ST_SIZE]
+        return self.obj.size_bytes
 
     def get_content_type(self):
-        return util.guess_mime_type(self.path)
+        return self.obj.content_type
 
     def get_creation_date(self):
-        return self.file_stat[stat.ST_CTIME]
+        return self.obj.epoch
 
     def get_display_name(self):
         return self.name
 
     def get_etag(self):
-        return util.get_etag(self._file_path)
+        return '{0}-{1}'.format(self.obj.epoch, self.obj.size_bytes)
 
     def get_last_modified(self):
-        return self.file_stat[stat.ST_MTIME]
+        return self.obj.epoch
 
     def support_etag(self):
         return True
@@ -75,38 +100,16 @@ class FileResource(DAVNonCollection):
         return True
 
     def get_content(self):
-        """Open content as a stream for reading.
-
-        See DAVResource.get_content()
-        """
         assert not self.is_collection
-        # GC issue 28, 57: if we open in text mode, \r\n is converted to one byte.
-        # So the file size reported by Windows differs from len(..), thus
-        # content-length will be wrong.
-        return open(self._file_path, "rb", BUFFER_SIZE)
+        return self.db._file_model.filestream_get(self.obj).read_stream_get()
 
     def begin_write(self, content_type=None):
-        """Open content as a stream for writing.
-
-        See DAVResource.begin_write()
-        """
-        assert not self.is_collection
-        if self.provider.readonly:
-            raise DAVError(HTTP_FORBIDDEN)
-        # _logger.debug("begin_write: {}, {}".format(self._file_path, "wb"))
-        # GC issue 57: always store as binary
-        return open(self._file_path, "wb", BUFFER_SIZE)
+        return self.db._file_model.filestream_get(self.obj)
 
     def delete(self):
-        """Remove this resource or collection (recursive).
-
-        See DAVResource.delete()
-        """
         if self.provider.readonly:
             raise DAVError(HTTP_FORBIDDEN)
-        os.unlink(self._file_path)
-        self.remove_all_properties(True)
-        self.remove_all_locks(True)
+        self.db.file_delete(self._file_path)
 
     def copy_move_single(self, dest_path, is_move):
         """See DAVResource.copy_move_single() """
@@ -114,55 +117,29 @@ class FileResource(DAVNonCollection):
             raise DAVError(HTTP_FORBIDDEN)
         fpDest = self.provider._loc_to_file_path(dest_path, self.environ)
         assert not util.is_equal_or_child_uri(self.path, dest_path)
-        # Copy file (overwrite, if exists)
-        shutil.copy2(self._file_path, fpDest)
-        # (Live properties are copied by copy2 or copystat)
-        # Copy dead properties
-        propMan = self.provider.prop_manager
-        if propMan:
-            destRes = self.provider.get_resource_inst(dest_path, self.environ)
-            if is_move:
-                propMan.move_properties(
-                    self.get_ref_url(),
-                    destRes.get_ref_url(),
-                    with_children=False,
-                    environ=self.environ,
-                )
-            else:
-                propMan.copy_properties(
-                    self.get_ref_url(), destRes.get_ref_url(), self.environ
-                )
+        self.db.file_copy_form_bcdbfs(self._file_path, fpDest)
+        if is_move:
+            self.db.file_delete(self._file_path)
 
     def support_recursive_move(self, dest_path):
-        """Return True, if move_recursive() is available (see comments there)."""
         return True
 
     def move_recursive(self, dest_path):
-        """See DAVResource.move_recursive() """
         if self.provider.readonly:
             raise DAVError(HTTP_FORBIDDEN)
         fpDest = self.provider._loc_to_file_path(dest_path, self.environ)
         assert not util.is_equal_or_child_uri(self.path, dest_path)
-        assert not os.path.exists(fpDest)
         _logger.debug("move_recursive({}, {})".format(self._file_path, fpDest))
-        shutil.move(self._file_path, fpDest)
-        # (Live properties are copied by copy2 or copystat)
-        # Move dead properties
-        if self.provider.prop_manager:
-            destRes = self.provider.get_resource_inst(dest_path, self.environ)
-            self.provider.prop_manager.move_properties(
-                self.get_ref_url(),
-                destRes.get_ref_url(),
-                with_children=True,
-                environ=self.environ,
-            )
+        self.db.file_copy_form_bcdbfs(self._file_path, fpDest)
+        self.db.file_delete(self._file_path)
+
 
     def set_last_modified(self, dest_path, time_stamp, dry_run):
         """Set last modified time for destPath to timeStamp on epoch-format"""
         # Translate time from RFC 1123 to seconds since epoch format
         secs = util.parse_time_string(time_stamp)
         if not dry_run:
-            os.utime(self._file_path, (secs, secs))
+            self.obj.epoch = int(secs)
         return True
 
 
@@ -178,15 +155,14 @@ class FolderResource(DAVCollection):
     def __init__(self, path, environ, file_path):
         super(FolderResource, self).__init__(path, environ)
         self._file_path = file_path
-        #        self._dict = None
-        self.file_stat = os.stat(self._file_path)
-        # Setting the name from the file path should fix the case on Windows
+        self.db = j.sal.bcdbfs
+        self.obj = self.db._dir_model.get_by_name(name=file_path)[0]
         self.name = os.path.basename(self._file_path)
         self.name = compat.to_native(self.name)  # .encode("utf8")
 
     # Getter methods for standard live properties
     def get_creation_date(self):
-        return self.file_stat[stat.ST_CTIME]
+        return self.obj.epoch
 
     def get_display_name(self):
         return self.name
@@ -198,30 +174,19 @@ class FolderResource(DAVCollection):
         return None
 
     def get_last_modified(self):
-        return self.file_stat[stat.ST_MTIME]
+        return self.obj.epoch
 
     def get_member_names(self):
-        """Return list of direct collection member names (utf-8 encoded).
-
-        See DAVCollection.get_member_names()
-        """
-        # On Windows NT/2k/XP and Unix, if path is a Unicode object, the result
-        # will be a list of Unicode objects.
-        # Undecodable filenames will still be returned as string objects
-        # If we don't request unicode, for example Vista may return a '?'
-        # instead of a special character. The name would then be unusable to
-        # build a distinct URL that references this resource.
-
         nameList = []
         # self._file_path is unicode, so os.listdir returns unicode as well
         assert compat.is_unicode(self._file_path)
-        for name in os.listdir(self._file_path):
+        for name in self.db.list_files_and_dirs(self._file_path):
             if not compat.is_unicode(name):
                 name = name.decode(sys.getfilesystemencoding())
             assert compat.is_unicode(name)
             # Skip non files (links and mount points)
-            fp = os.path.join(self._file_path, name)
-            if not os.path.isdir(fp) and not os.path.isfile(fp):
+            fp = name
+            if not self.db.is_dir(fp) and not self.db.is_file(fp):
                 _logger.debug("Skipping non-file {!r}".format(fp))
                 continue
             # name = name.encode("utf8")
@@ -230,93 +195,57 @@ class FolderResource(DAVCollection):
         return nameList
 
     def get_member(self, name):
-        """Return direct collection member (DAVResource or derived).
-
-        See DAVCollection.get_member()
-        """
         assert compat.is_native(name), "{!r}".format(name)
-        fp = os.path.join(self._file_path, compat.to_unicode(name))
+        fp = compat.to_unicode(name)
         #        name = name.encode("utf8")
-        path = util.join_uri(self.path, name)
-        if os.path.isdir(fp):
+        path = name
+        if self.db.is_dir(fp):
             res = FolderResource(path, self.environ, fp)
-        elif os.path.isfile(fp):
+        elif self.db.is_file(fp):
             res = FileResource(path, self.environ, fp)
         else:
             _logger.debug("Skipping non-file {}".format(path))
             res = None
+
         return res
 
     # --- Read / write -------------------------------------------------------
 
     def create_empty_resource(self, name):
-        """Create an empty (length-0) resource.
-
-        See DAVResource.create_empty_resource()
-        """
         assert "/" not in name
         if self.provider.readonly:
             raise DAVError(HTTP_FORBIDDEN)
         path = util.join_uri(self.path, name)
         fp = self.provider._loc_to_file_path(path, self.environ)
-        f = open(fp, "wb")
-        f.close()
+        self.db.file_create_empty(fp)
         return self.provider.get_resource_inst(path, self.environ)
 
     def create_collection(self, name):
-        """Create a new collection as member of self.
-
-        See DAVResource.create_collection()
-        """
         assert "/" not in name
         if self.provider.readonly:
             raise DAVError(HTTP_FORBIDDEN)
         path = util.join_uri(self.path, name)
         fp = self.provider._loc_to_file_path(path, self.environ)
-        os.mkdir(fp)
+        self.db.dir_create(fp)
 
     def delete(self):
-        """Remove this resource or collection (recursive).
-
-        See DAVResource.delete()
-        """
         if self.provider.readonly:
             raise DAVError(HTTP_FORBIDDEN)
-        shutil.rmtree(self._file_path, ignore_errors=False)
-        self.remove_all_properties(True)
-        self.remove_all_locks(True)
+        self.db.dir_remove(self._file_path)
 
     def copy_move_single(self, dest_path, is_move):
         """See DAVResource.copy_move_single() """
         if self.provider.readonly:
             raise DAVError(HTTP_FORBIDDEN)
         fpDest = self.provider._loc_to_file_path(dest_path, self.environ)
+        self.db.dir_copy_from_bcdbfs(self._file_path, dest_path)
         assert not util.is_equal_or_child_uri(self.path, dest_path)
         # Create destination collection, if not exists
-        if not os.path.exists(fpDest):
-            os.mkdir(fpDest)
-        try:
-            # may raise: [Error 5] Permission denied:
-            # u'C:\\temp\\litmus\\ccdest'
-            shutil.copystat(self._file_path, fpDest)
-        except Exception:
-            _logger.exception("Could not copy folder stats: {}".format(self._file_path))
-        # (Live properties are copied by copy2 or copystat)
-        # Copy dead properties
-        propMan = self.provider.prop_manager
-        if propMan:
-            destRes = self.provider.get_resource_inst(dest_path, self.environ)
-            if is_move:
-                propMan.move_properties(
-                    self.get_ref_url(),
-                    destRes.get_ref_url(),
-                    with_children=False,
-                    environ=self.environ,
-                )
-            else:
-                propMan.copy_properties(
-                    self.get_ref_url(), destRes.get_ref_url(), self.environ
-                )
+
+        if is_move:
+            self.db.dir_remove(self._file_path)
+
+
 
     def support_recursive_move(self, dest_path):
         """Return True, if move_recursive() is available (see comments there)."""
@@ -328,41 +257,36 @@ class FolderResource(DAVCollection):
             raise DAVError(HTTP_FORBIDDEN)
         fpDest = self.provider._loc_to_file_path(dest_path, self.environ)
         assert not util.is_equal_or_child_uri(self.path, dest_path)
-        assert not os.path.exists(fpDest)
         _logger.debug("move_recursive({}, {})".format(self._file_path, fpDest))
-        shutil.move(self._file_path, fpDest)
+        self.db.dir_copy_from_bcdbfs(self._file_path, fpDest)
+        self.db.dir_remove(self._file_path)
         # (Live properties are copied by copy2 or copystat)
         # Move dead properties
-        if self.provider.prop_manager:
-            destRes = self.provider.get_resource_inst(dest_path, self.environ)
-            self.provider.prop_manager.move_properties(
-                self.get_ref_url(),
-                destRes.get_ref_url(),
-                with_children=True,
-                environ=self.environ,
-            )
 
     def set_last_modified(self, dest_path, time_stamp, dry_run):
         """Set last modified time for destPath to timeStamp on epoch-format"""
         # Translate time from RFC 1123 to seconds since epoch format
         secs = util.parse_time_string(time_stamp)
         if not dry_run:
-            os.utime(self._file_path, (secs, secs))
+            obj = self.db._dir_model.get_by_name(name=dest_path)[0]
+            obj.epoch = int(secs)
+            obj.save()
         return True
 
 
 # ========================================================================
 # FilesystemProvider
 # ========================================================================
-class FilesystemProvider(DAVProvider):
+class BCDBFSProvider(DAVProvider):
     def __init__(self, root_folder_path, readonly=False):
         # Expand leading '~' as user home dir; expand %VAR%, $Var, ..
         root_folder_path = os.path.expandvars(os.path.expanduser(root_folder_path))
         root_folder_path = os.path.abspath(root_folder_path)
-        if not root_folder_path or not os.path.exists(root_folder_path):
+        self.db = j.sal.bcdbfs
+        if not root_folder_path or not self.db.dir_exists(root_folder_path):
             raise ValueError("Invalid root path: {}".format(root_folder_path))
 
-        super(FilesystemProvider, self).__init__()
+        super(BCDBFSProvider, self).__init__()
 
         self.root_folder_path = root_folder_path
         self.readonly = readonly
@@ -396,7 +320,7 @@ class FilesystemProvider(DAVProvider):
 
         # Convert to unicode
         file_path = util.to_unicode_safe(file_path)
-        return file_path
+        return file_path.rstrip('/') or '/'
 
     def is_readonly(self):
         return self.readonly
@@ -408,9 +332,9 @@ class FilesystemProvider(DAVProvider):
         """
         self._count_get_resource_inst += 1
         fp = self._loc_to_file_path(path, environ)
-        if not os.path.exists(fp):
+        if not self.db.exists(fp):
             return None
 
-        if os.path.isdir(fp):
+        if self.db.is_dir(fp):
             return FolderResource(path, environ, fp)
         return FileResource(path, environ, fp)
